475597550af8e65eb47afa6245670f1705042d0b
Chris Malcolm <chrisishere@Chriss-MacBook-Pro.local>
2018-01-31 02:51:43 -0800

    updated README

--- README.md
+++ README.md
@@ -1,39 +1,6 @@
-# node-js-getting-started
+# About
 
-A barebones Node.js app using [Express 4](http://expressjs.com/).
+Simple sum web app in node.js because mm why not, eh? Posts to /test route given an x + y value, adds the two numbers. 
 
-This application supports the [Getting Started with Node on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs) article - check it out.
+View demo here (https://rocky-atoll-30961.herokuapp.com/).
 
-## Running Locally
-
-Make sure you have [Node.js](http://nodejs.org/) and the [Heroku CLI](https://cli.heroku.com/) installed.
-
-```sh
-$ git clone git@github.com:heroku/node-js-getting-started.git # or clone your own fork
-$ cd node-js-getting-started
-$ npm install
-$ npm start
-```
-
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-## Deploying to Heroku
-
-```
-$ heroku create
-$ git push heroku master
-$ heroku open
-```
-or
-
-[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## Documentation
-
-For more information about using Node.js on Heroku, see these Dev Center articles:
-
-- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
-- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
-- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
-- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
-- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const express = require('express');
-//const path = require('path');
-//const PORT = process.env.PORT || 5000;
+const path = require('path');
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-app.get("/"), function(req, res){
-	res.send("this is home");
-});
-app.get("/test", function(req, res){
-	res.send("this is a test page.");	
-})
-/*
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-*/
-app.listen(5000)
+var helperUtils = {
+	//validates string as a number
+	validateNum: function(str){
+		str ==  (typeof str == "string") ? str.trim() : str;
+		var res = parseFloat(str);
+		return str=="" || isNaN(parseFloat(res)) ? false : res;
+	}
+}
+
+app
+	.use(bodyParser.urlencoded({extended: true}))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get("/", function(req, res){
+		res.render('pages/index');
+
+	})
+	.post("/test", function(req, res){
+		res.setHeader('content-type', 'application/json');
+		//retrieve nums if string
+		var trueNums = {
+			x: helperUtils.validateNum(req.body.x),
+			y: helperUtils.validateNum(req.body.y)
+		}
+		//give error or sum
+		var result = (trueNums.x===false || trueNums.y ===false) ? 
+			"One or both of your numbers are invalid!" : 
+			(trueNums.x+trueNums.y);
+		
+		res.send(JSON.stringify({
+			_x: req.body.x, 
+			_y: req.body.y, 
+			parsedX: trueNums.x, 
+			parsedY: trueNums.y, 
+			sum: result}));
+	})
+	.listen(PORT);
