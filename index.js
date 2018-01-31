0ed8e2db18d818ec735d10ee8d6f5fa896c22716
Chris Malcolm <chrisishere@Chriss-MacBook-Pro.local>
2018-01-31 01:36:23 -0800

    edit server stuff.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+//const path = require('path');
+//const PORT = process.env.PORT || 5000;
 
+var app = express();
+
+app.get("/"), function(req, res){
+	res.send("this is home");
+});
+app.get("/test", function(req, res){
+	res.send("this is a test page.");	
+})
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
